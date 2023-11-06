75a16c8b4d97b0e0547707c9b447d76ff2c34746
Emma Kelminson <ekel@Emmas-MacBook-Pro.local>
2023-11-06 15:52:58 -0500

    restoring old version for organization

---
+++ src/main/scala/Minimize.scala
@@ -0,0 +1,7 @@
+//object Minimize {
+//  def minimizeGraph(eBNFA: ExtendedBooleanNFA): ExtendedBooleanNFA = {
+//    val connectionsWithoutFlipCoin = eBNFA.connections.map{ case(node, connection, condition) =>
+//
+//    }
+//  }
+//}
